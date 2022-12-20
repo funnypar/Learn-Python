57a8bb3074704b73f2abfb5ffaf278ee3f7084b0
Mopano <99732517+funnypar@users.noreply.github.com>
2022-12-20 06:10:45 -0800

    Add files via upload

---
+++ sixth(game)/README.md
@@ -0,0 +1,15 @@
+## Lessons Learned
+
+In this exercice we want to solve the third exercice of "Jadi course" :
+
+Question :
+
+🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨
+
+Write a game🎮 which pc choose a number in range(1,99) and user must guess the number ! User just has 5 life 😊. Try to make your game beautiful 👽🤪
+
+🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨
+
+Answer :
+
+Look at the sixth.py 😊
---
+++ sixth(game)/sixth.py
@@ -0,0 +1,38 @@
+import random
+
+#Mopano function ***************************************************************
+def game(name):
+    pcAnswer = random.randint(1,99)
+    print(pcAnswer)
+    print("Okay ",name, " Let's Goooooo!!!!\n\n")
+    userAnswer = int(input("Please Enter your guess 👽:  \n"))
+    i = 5
+    while userAnswer != pcAnswer and i > 1 :
+        if pcAnswer > userAnswer:
+            print("mine is higher 😈")
+            i -=1
+            userAnswer = int(input("\n\nPlease Enter your guess again 😁:  \n"))
+        elif pcAnswer < userAnswer:
+            print("mine is smaller 🤦‍♂️")
+            i -=1
+            userAnswer = int(input("\n\nPlease Enter your guess again 😁:  \n"))
+    if pcAnswer == userAnswer:
+        print(name, "  Woooowwwww! you did that 💥❤️😍")
+    else:
+        print("You are a loserrrrrrrrr 😈😈😈" , name)
+
+
+#Start Game ********************************************************************
+
+print("------------------------------------------ Guess game 🎮 -----------------------------------------------")
+LikeToPaly = input("Would you like to play ? (yes😊 : y  ,  no 😒 : n)\n\n")
+if LikeToPaly == "y" :
+    name = input("\nWhat is your name ? \n\n")
+    game(name)
+    playAgain = input("\nWould you like to play again ? 😊 (yes😊 : y  ,  no 😒 : n)\n\n")
+    while playAgain == "y":
+        game(name)
+        playAgain = input("\nWould you like to play again ? 😊 (yes😊 : y  ,  no 😒 : n)\n\n")
+    print("\n\nYou are a loserrrrrrrrr 😈😈😈" , name)
+else:
+    print("\n\nOkey loser 😵🤪")
