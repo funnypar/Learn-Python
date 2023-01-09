33ec35af2fbaca5f6ca7cdedd81c95aa1dfac93c
Mopano <99732517+funnypar@users.noreply.github.com>
2023-01-09 01:14:26 -0800

    Add files via upload

--- TwentyFourth/TwentyFourth.py
+++ TwentyFourth/TwentyFourth.py
@@ -5,12 +5,12 @@ import re
 res = requests.get("https://exo.ir/category/%DA%A9%DB%8C%D8%A8%D9%88%D8%B1%D8%AF-%DA%AF%DB%8C%D9%85%DB%8C%D9%86%DA%AF?sort=p.price&order=ASC&fq=1")
 soup = BeautifulSoup(res.text, "html.parser")
 values = soup.find_all("div",{"class":"caption"})
-for value in values: 
-    print(value.text)
-
-
-
-
+for value in values:
+    newValue = re.sub(r"\n"," ",value.text)
+    #brand = re.findall(r"تولیدکننده: (\w*-*\w+)",newValue))
+    #model = re.findall(r"کد محصول: (.+?(?=[کیبورد پد]))",newValue))
+    #price = re.findall(r"\d+,\d+,*\d+",newValue)
+    print(price)
 
 
 
